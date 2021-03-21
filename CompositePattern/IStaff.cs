292add1331626aa590f9272f50eb7272c9352008
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-21 14:22:41 +0300

    Add files via upload

---
+++ CompositePattern/IStaff.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositePattern
+{
+    interface IStaff
+    {
+        void Details();
+    }
+}
---
+++ CompositePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CompositePattern.Composites;
+using CompositePattern.Leaves;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IStaff Elif = new Employee("Elif", "HR");
+            IStaff Canan = new Employee("Canan", "IT");
+            IStaff Emin = new Employee("Emin", "FR");
+            IStaff Kadir = new Employee("Kadir", "AC");
+
+            IStaff Gamze = new Manager("Gamze", "IT"){subOrdinates = {Canan,Kadir}};
+            IStaff Julia = new Manager("Julia", "HR"){subOrdinates = {Elif,Emin}};
+
+            Elif.Details();
+
+            Console.WriteLine();
+        }
+    }
+}
