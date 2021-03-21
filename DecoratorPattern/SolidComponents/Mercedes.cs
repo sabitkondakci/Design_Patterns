e679448ee82c4e24c784f83ffb7cb729c561122b
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-21 15:53:24 +0300

    Add files via upload

---
+++ DecoratorPattern/SolidComponents/Mercedes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPattern.SolidComponents
+{
+    // sealed to prevent modification
+    sealed class Mercedes:ICar
+    {
+        public double Price => 33.421;
+        public string Model => "Benz W115";
+    }
+}
