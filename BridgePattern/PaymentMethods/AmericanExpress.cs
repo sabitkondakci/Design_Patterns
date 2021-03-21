0c5589d310393a045655ae9dda51c20da8608b82
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-21 12:22:07 +0300

    Create AmericanExpress.cs

---
+++ BridgePattern/PaymentMethods/AmericanExpress.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePatternV2.PaymentMethods
+{
+    class AmericanExpress:Payment
+    {
+        public override void Pay()
+        {
+            PaymentMedium.StartTransaction("American Express");
+            Console.WriteLine("AmericanExpress Payment Method Invoked");
+        }
+    }
+}
