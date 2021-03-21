5d50cf27d0ad808b67fce3171edb0649728080a1
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-21 15:56:00 +0300

    Add files via upload

---
+++ DecoratorPattern/ICar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public interface ICar
+    {
+        double Price { get; }
+        string Model { get; }
+    }
+}
---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DecoratorPattern.Decorator;
+using DecoratorPattern.SolidComponents;
+using DecoratorPattern.SolidDecorator;
+
+namespace DecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new Mercedes();
+            CarDecorator decorator = new AfterDecoration(car);
+
+            Console.WriteLine($"Name:{car.GetType().Name}\nModel:{decorator.Model}\nPrice:{decorator.Price}\nOff-Price:{decorator.DiscountedPrice()}");
+        }
+    }
+}
