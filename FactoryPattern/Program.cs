040dd499d447cac665cd9624620a0236b3370e95
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 23:11:17 +0300

    Add files via upload

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FactoryPattern.Factories;
+using FactoryPattern.Interfaces;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVehicle vehicle = VehicleFactory.Create(Vehicles.Mercedes);
+            IAnimal animal = AnimalFactory.Create(Animals.Raven);
+
+            Console.WriteLine(vehicle.Model);
+            Console.WriteLine(vehicle.Price);
+
+            animal.AnimalFamily();
+            animal.AnimalSound();
+        }
+    }
+}
