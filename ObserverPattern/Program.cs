5bd794430bf6c91d29c1c864b0b74eed12356a01
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 20:11:29 +0300

    Add files via upload

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ObserverPattern.Interfaces;
+using ObserverPattern.Observables;
+using ObserverPattern.Observers;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable stockControl = new StockControl();
+            Observer electronicShelfLabel = new ElectronicShelfLabel();
+            Observer managerDesktopDisplay = new ManagerDesktopDisplay();
+
+            stockControl.AddAsync(electronicShelfLabel);
+            stockControl.AddAsync(managerDesktopDisplay);
+
+            stockControl.RefreshPrice(11).GetAwaiter().GetResult();
+            stockControl.NotifyAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("--- Before Update ---");
+            Console.WriteLine(electronicShelfLabel.updatedPrice);
+            Console.WriteLine(managerDesktopDisplay.updatedPrice);
+
+
+            stockControl.RefreshPrice(25).GetAwaiter().GetResult();
+            stockControl.RemoveAsync(managerDesktopDisplay).GetAwaiter().GetResult();
+            stockControl.NotifyAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("--- After Update ---");
+            Console.WriteLine(electronicShelfLabel.updatedPrice);
+            Console.WriteLine(managerDesktopDisplay.updatedPrice);
+
+
+
+        }
+    }
+}
