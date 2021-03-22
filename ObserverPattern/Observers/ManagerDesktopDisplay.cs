82eaba588162e4bf8a68e5b41557604939020ece
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 20:10:12 +0300

    Add files via upload

---
+++ ObserverPattern/Observers/ManagerDesktopDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ObserverPattern.Interfaces;
+
+namespace ObserverPattern.Observers
+{
+    class ManagerDesktopDisplay : Observer
+    {
+        public override async Task Update(Observable stockControl)
+        {
+            updatedPrice = await stockControl.UpdatedPriceAsync();
+        }
+    }
+}
