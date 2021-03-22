42eed97b0f288e1666f13af4da873d3754a21a56
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 23:09:45 +0300

    Create IAnimal.cs

---
+++ FactoryPattern/Interfaces/IAnimal.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern.Interfaces
+{
+    interface IAnimal
+    {
+        void AnimalSound();
+        void AnimalFamily();
+    }
+}
