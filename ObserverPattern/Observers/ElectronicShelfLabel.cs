c81e95f6d07dd1da6eecf07d4f2a76692d13b582
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 20:15:01 +0300

    Update ElectronicShelfLabel.cs

--- ObserverPattern/Observers/ElectronicShelfLabel.cs
+++ ObserverPattern/Observers/ElectronicShelfLabel.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using ObserverPattern.Interfaces;
+using ObserverPattern.Abstracts;
 
 namespace ObserverPattern.Observers
 {
