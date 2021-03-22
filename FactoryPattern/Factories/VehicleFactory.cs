65f2ac74cd1c92dd89d52753e82829c54600b003
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 23:08:59 +0300

    Add files via upload

---
+++ FactoryPattern/Factories/VehicleFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FactoryPattern.Interfaces;
+using FactoryPattern.SolidComponents;
+
+namespace FactoryPattern.Factories
+{
+    public enum Vehicles
+    {
+        Mitsubishi,
+        Mercedes
+    };
+    struct VehicleFactory
+    {
+        public static IVehicle Create(Vehicles vehicle)
+        {
+            switch (vehicle)
+            {
+                case Vehicles.Mitsubishi:
+                    return new Mitsubishi();
+                case Vehicles.Mercedes:
+                    return new Mercedes();
+                default:
+                    return null;
+            }
+        }
+    }
+}
