80a26054d6e4dd359eb0064245475eaf3f17b268
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 20:07:35 +0300

    Add files via upload

---
+++ ObserverPattern/Abstracts/Observable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Interfaces
+{
+    abstract class  Observable
+    {
+        protected List<Observer> observerList;
+
+        protected double updatedPrice;
+        protected Observable()
+        {
+            this.observerList = new List<Observer>();
+        }
+        public abstract Task AddAsync(Observer observer);
+        public abstract Task RemoveAsync(Observer observer);
+        public abstract Task NotifyAsync();
+        public abstract Task<double> UpdatedPriceAsync();
+        public async Task RefreshPrice(double price)
+        {
+            await Task.Run(() =>
+            {
+                updatedPrice = price;
+            });
+        }
+    }
+}
