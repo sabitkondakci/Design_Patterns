ed715ef7cc315c65ee0b5761937ec39b1fce451f
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-22 23:10:00 +0300

    Add files via upload

---
+++ FactoryPattern/Interfaces/IVehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern.Interfaces
+{
+    interface IVehicle
+    {
+        string Model { get; set; }
+        double Price { get; set; }
+
+    }
+}
