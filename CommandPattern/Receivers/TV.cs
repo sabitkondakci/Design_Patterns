40df2edf0659f8dc3bdaae032fe93eadf8b432d6
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-23 11:00:54 +0300

    Add files via upload

---
+++ CommandPattern/Receivers/TV.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Receivers
+{
+    class TV
+    {
+        public void TvAction()
+        {
+            Console.WriteLine("TV Action");
+        }
+    }
+}
