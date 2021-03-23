0c496515201c15628d7aa9f37e5de77626eeda7a
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-23 11:00:29 +0300

    Create MusicPlayer.cs

---
+++ CommandPattern/Receivers/MusicPlayer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Receivers
+{
+    class MusicPlayer
+    {
+        public void MediaAction()
+        {
+            Console.WriteLine("Music Player Action");
+        }
+    }
+}
