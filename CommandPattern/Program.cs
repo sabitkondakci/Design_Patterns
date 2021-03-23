d65207de876dff6ed644e6c90e6181268938fe95
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-23 11:01:16 +0300

    Add files via upload

---
+++ CommandPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommandPattern.Commands;
+using CommandPattern.Invokers;
+
+namespace CommandPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var tvController = new TVRemoteController(new MediaButtonControls(), new NumericButtonControls());
+            var mediaController = new PlayerController(new MediaButtonControls());
+
+            tvController.SwitchChannel(1); //TV Action
+            tvController.StitchMediaPlayer(2); //Music Player Action
+
+            mediaController.Media(1); //Music Player Action
+            Console.Read();
+        }
+    }
+}
