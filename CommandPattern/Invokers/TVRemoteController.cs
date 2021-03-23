39393cea32f6d67d2490aa47236614d204c22cc7
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-23 10:59:41 +0300

    Add files via upload

---
+++ CommandPattern/Invokers/TVRemoteController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Invokers
+{
+    class TVRemoteController
+    {
+        private readonly ICommand _mediaButtonControls;
+        private readonly ICommand _numericButtonControls;
+
+        public TVRemoteController(ICommand mediaButtonControls,ICommand numericButtonControls)
+        {
+            this._mediaButtonControls = mediaButtonControls;
+            this._numericButtonControls = numericButtonControls;
+        }
+
+        public void SwitchChannel(int channel)
+        {
+            // specific channel
+            try
+            {
+                _numericButtonControls.Execute(); // channel switch
+            }
+            catch 
+            {
+               _numericButtonControls.Revoke();
+            }
+            
+        }
+
+        public void StitchMediaPlayer(int media)
+        {
+            // specific media button
+            try
+            {
+                _mediaButtonControls.Execute();
+            }
+            catch 
+            {
+                _mediaButtonControls.Revoke();
+            }
+           
+        }
+    }
+}
