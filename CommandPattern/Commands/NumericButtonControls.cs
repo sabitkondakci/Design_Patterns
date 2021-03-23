07dd44fc365acd137381d367d8a5538f9fe7e345
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-23 10:58:39 +0300

    Add files via upload

---
+++ CommandPattern/Commands/NumericButtonControls.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandPattern.Receivers;
+
+namespace CommandPattern.Commands
+{
+    class NumericButtonControls:TV,ICommand
+    {
+        public void Execute()
+        {
+            TvAction();
+        }
+
+        public void Revoke()
+        {
+            Console.WriteLine("Numeric Button Control Has Been Revoked");
+        }
+    }
+}
