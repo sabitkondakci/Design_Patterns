1f525522a6bde3b033148f042df8f6a2df449c9b
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-24 17:13:42 +0300

    Add files via upload

---
+++ TemplatePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TemplatePattern.AbstractTemplates;
+using TemplatePattern.SolidComponents;
+
+namespace TemplatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseManager dbSQLManager = new SQLDatabase();
+            DatabaseManager dbOracleManager = new OracleDatabase();
+
+            dbSQLManager.SaveToDatabase();
+            dbOracleManager.SaveToDatabase();
+            
+        }
+    }
+}
