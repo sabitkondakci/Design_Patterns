3453a49fb3fee819351680aa433a36576f904df9
Sabit Kondakçı <kondakcisabit@gmail.com>
2021-03-24 17:13:14 +0300

    Add files via upload

---
+++ TemplatePattern/SolidComponents/SQLDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TemplatePattern.AbstractTemplates;
+
+namespace TemplatePattern.SolidComponents
+{
+    class SQLDatabase:DatabaseManager
+    {
+        protected override void Save()
+        {
+            //operations ...
+            Console.WriteLine("Saved to Microsoft SQL Database");
+        }
+    }
+}
