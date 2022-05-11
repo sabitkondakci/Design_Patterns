611c5a6bf0f67ca561331b3da1055f0358a3a82b
Sabit Kondakçı <kondakcisabit@gmail.com>
2022-05-11 17:09:46 +0300

    Update Lazy<Singleton>.cs

--- SingletonDesign/Lazy<Singleton>.cs
+++ SingletonDesign/Lazy<Singleton>.cs
@@ -1,10 +1,10 @@
- //sealed , to prevent a nested class inheriting this class
+ //sealed , to prevent a nested class from inheriting this class
     sealed class SingletonDesign
     {
         // Lazy<> class ensures thread safety , .Net 4.0+
-        private static readonly Lazy<SingletonDesign> _singleton = new Lazy<SingletonDesign>(()=>new SingletonDesign());
+        private static readonly Lazy<SingletonDesign> _singleton = new Lazy<SingletonDesign>(()=>new SingletonDesign(), LazyThreadSafetyMode.ExecutionAndPublication);
 
-        //private constructor to prevent other classes creating an instance from this class
+        //private constructor to prevent other classes from creating an instance of this class
         private SingletonDesign() { }
 
         public static SingletonDesign GetInstance => _singleton.Value;
